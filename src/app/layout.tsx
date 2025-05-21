d45d15893e5290f9bd1b0fe829212d0c1024fd74
smllns <marysmoly@gmail.com>
2025-05-22 02:07:48 +0300

    meta tags updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,29 @@ export const metadata: Metadata = {
   title: 'Smllns Film Club',
   description:
     'Smllns Film Club — where code meets creativity and passion for movies. Explore a unique movie collection with a visually engaging experience.',
+  openGraph: {
+    title: 'Smllns Film Club',
+    description:
+      'A personal movie archive with animations, TMDb API and love for cinema.',
+    url: 'https://smllns-film-club.vercel.app/',
+    siteName: 'Smllns Film Club',
+    images: [
+      {
+        url: '/preview.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Smllns Film Club',
+    description:
+      'A personal movie archive with animations, TMDb API and love for cinema.',
+    images: ['/preview.jpg'],
+  },
 };
 
 export default function RootLayout({
